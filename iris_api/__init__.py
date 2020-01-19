d68d6911e46190d6253e4b046f31b57f7459bae7
Curtly <curtlyblows@gmail.com>
2020-01-19 18:09:04 +0200

    add model stored path to package environment

--- iris_api/__init__.py
+++ iris_api/__init__.py
@@ -1,7 +1,7 @@
-import os
+from os import path
 import pkg_resources
 
-MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
-MODEL_PATH = os.path.join(MODULE_PATH, 'model_store')
+MODULE_PATH = path.dirname(path.realpath(__file__))
+MODEL_PATH = path.join(MODULE_PATH, 'model_store')
 
 VERSION = pkg_resources.get_distribution(__name__).version
