f1b13831573cd0afa5c7b0eb3ba2de27f1d3de41
Curtly <curtlyblows@gmail.com>
2020-01-19 18:11:11 +0200

    training file, to store model pkl

--- train/train.py
+++ train/train.py
@@ -1,4 +1,16 @@
 from iris_api import MODEL_PATH
-import joblib
-from os.path import realpath, dirname, join
+import pandas as pd
 from sklearn import datasets
+from iris_api.model import IrisPipeline
+import numpy as np
+
+iris = datasets.load_iris()
+df = pd.DataFrame(data=np.c_[iris['data'], iris['target']],
+                  columns=iris['feature_names'] + ['target'])
+
+converter = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+df.replace({'target': converter}, inplace=True)
+
+model = IrisPipeline()
+model.fit(df.drop(['target']), df['target'])
+model.save(directory=MODEL_PATH)
